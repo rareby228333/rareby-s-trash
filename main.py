1e9d3c53c167d737cde8fc1a9e0215d95f192423
rareby <tolstovkira1@gmail.com>
2025-12-29 15:18:57 +0500

    123123

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-class Locker:
-    x = 10
-    def f1(self):
-        x=15
-        print(x)
-        f2()
-
-        some_object = Locker()
-        some_object.f1()
+class student:
+ name = "student"
+ def __init__(self):
+     self.height = 180
+     self.weight = 75
+     self.name = "Aleksandr"
+     self.age = 22
+     print("hello, teacher!")
